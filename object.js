2fd6f95991bb7927b92c1f13f3253fd1d07b3b89
Uhamzat105 <Uhamzat105@gmail.com>
2025-04-05 20:31:22 +0100

    assignme

---
+++ object.js
@@ -0,0 +1,12 @@
+const book = {
+    title: "story of my life",
+    description: "all about me",
+    numberofpage : 360,
+    author: "hamzat usman opeyemi",
+    reading: false
+};
+
+console.log(book);
+
+    
+
