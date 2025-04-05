d8ec2d6a2bf0ec3d545a52f5fc8fdc8f1381a34c
Uhamzat105 <Uhamzat105@gmail.com>
2025-04-05 11:01:44 +0100

    odd   number

--- odd.js
+++ odd.js
@@ -4,3 +4,11 @@ for (let x = 1; x <= 20; x++)  {
         console.log(x) ;
     }
 }
+
+const bestfriend = ["ADE", "OLA", "BOLA", "TOLA", "FOLA"];
+
+console.log(bestfriend);
+
+const bestfriends = ["ADE", "OLA", "BOLA", "TOLA", "FOLA"];
+
+console.log("my best friend is " + bestfriend[2]);
