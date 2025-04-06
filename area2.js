e54f8210249beb63008f6ba211f97b5cd49a40e5
Uhamzat105 <Uhamzat105@gmail.com>
2025-04-06 21:21:17 +0100

    assign

---
+++ area2.js
@@ -0,0 +1,7 @@
+function
+calculateAreaOfRectantangle(length,width) {
+    const area = length * width; return area
+    }
+    const rectangleArea = calculateAreaOfRectantangle(5,3);
+    console.log(`the area of the rectangle is : ${rectangleArea}`);
+    
