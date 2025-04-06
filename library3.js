afaee4bdc43efb84db3a19e1c58817250a5e0141
Uhamzat105 <Uhamzat105@gmail.com>
2025-04-06 20:18:42 +0100

    assignment

---
+++ library3.js
@@ -0,0 +1,22 @@
+const books = [
+
+    {title: "LAGOS",
+        description: "city of execellent",
+        numerOfpages: 20,
+        author: "governor",
+        reading: "true"
+    },
+    {title: "oyo",
+        description: "action city",
+        numberOfpages: 14,
+        author: "king",
+        reading: "true"
+    },
+    {title: "abuja",
+        description: "federal capital",
+        numberOfpages: 30,
+        author: " president",
+        reading: "true",
+    }
+];
+console.log(books)
